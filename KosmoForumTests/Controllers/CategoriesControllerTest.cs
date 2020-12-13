e14dcbe0cf2349114e53b9f31b894c62452ceb2b
LukaszZietek <lukaszzietekk@gmail.com>
2020-12-13 16:54:08 +0100

    Add few tests to kosmoforum Web Api
    It is time to learn xUnit library and test WebApi.

---
+++ KosmoForumTests/Controllers/CategoriesControllerTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using KosmoForum.Controllers;
+using KosmoForum.Mapper;
+using KosmoForum.Models;
+using KosmoForum.Models.Dtos;
+using KosmoForum.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace KosmoForumTests.Controllers
+{
+    public class CategoriesControllerTest
+    {
+        private readonly Mock<ICategoryRepo> _mockRepo;
+        private readonly Mock<IMapper> _mockMapper;
+        private readonly CategoriesController _controller;
+
+        public CategoriesControllerTest()
+        {
+            _mockRepo = new Mock<ICategoryRepo>();
+            _mockMapper = new Mock<IMapper>();
+            _controller = new CategoriesController(_mockRepo.Object,_mockMapper.Object);
+        }
+
+        [Fact]
+        public void GetCategories_WithEmptyDbShouldReturnNotFound()
+        {
+            var result = _controller.GetCategories();
+
+            Assert.IsType<NotFoundObjectResult>(result);
+        }
+
+        [Fact]
+        public void GetCategories_With2ElementShouldReturn2ElementAndOkResponse()
+        {
+            _mockRepo.Setup(repo => repo.GetAllCategories())
+                .Returns(new List<Category>() { new Category() {CreationDateTime = DateTime.Now, Description = "Ala ma kota", Id = 0, Title = "Włosy"},
+                    new Category() {CreationDateTime = DateTime.Now, Description = "Kot ma ale", Id = 1, Title = "Głowa"}});
+
+            var result = _controller.GetCategories();
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var categories = Assert.IsType<List<CategoryDto>>(okResult.Value);
+            Assert.Equal(2, categories.Count);
+        }
+    }
+}
---
+++ KosmoForumTests/FakeRepo/CategoryFakeRepo.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KosmoForum.Models;
+using KosmoForum.Repository.IRepository;
+
+namespace KosmoForumTests.FakeRepo
+{
+    class CategoryFakeRepo : ICategoryRepo
+    {
+        private readonly List<Category> _categoriesList;
+
+        public CategoryFakeRepo()
+        {
+            _categoriesList = new List<Category>()
+            {
+                new Category() {CreationDateTime = DateTime.Now, Description = "Ala ma kota", Id = 0, Title = "Włosy"},
+                new Category() {CreationDateTime = DateTime.Now, Description = "Kot ma ale", Id = 1, Title = "Głowa"},
+                new Category() {CreationDateTime = DateTime.Now, Description = "Transformers", Id = 2, Title = "Nogi"},
+                new Category() {CreationDateTime = DateTime.Now, Description = "Czara ognia", Id = 3, Title = "Uszy"},
+            };
+        }
+
+        public ICollection<Category> GetAllCategories()
+        {
+            return _categoriesList.OrderBy(x => x.Title).ToList();
+        }
+
+        public Category GetCategory(int id)
+        {
+            return _categoriesList.FirstOrDefault(x => x.Id == id);
+        }
+
+        public Category GetCategory(string title)
+        {
+            var obj = _categoriesList.FirstOrDefault(x => x.Title.Trim().ToLower() == title.Trim().ToLower());
+            return obj;
+        }
+
+        public bool CategoryExists(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CategoryExists(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CreateCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
